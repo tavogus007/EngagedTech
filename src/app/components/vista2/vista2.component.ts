c3d19c6117414e2b2193d63ca119c0906f392734
tavogus007 <gustavog095065@gmail.com>
2026-01-29 20:34:39 -0400

    Feat: :art: Adding validations

--- src/app/components/vista2/vista2.component.ts
+++ src/app/components/vista2/vista2.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { EmailService } from '../../services/email.service';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-vista2',
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   standalone: true,
   templateUrl: './vista2.component.html',
   styleUrl: './vista2.component.css',
@@ -13,13 +14,27 @@ export class Vista2Component {
   nombre = '';
   email = '';
 
+  nombreInvalido = false;
+  emailInvalido = false;
+
   constructor(private emailService: EmailService) {}
 
+  validarNombre() {
+    this.nombreInvalido = this.nombre.length < 3 && this.nombre.length > 0;
+  }
+
+  validarEmail() {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    this.emailInvalido = !emailRegex.test(this.email) && this.email.length > 0;
+  }
+
+  get formularioValido() {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return this.nombre.length >= 3 && emailRegex.test(this.email);
+  }
+
   enviarCorreo() {
-    if (!this.nombre || !this.email) {
-      alert('Completa ambos campos');
-      return;
-    }
+    if (!this.formularioValido) return;
 
     this.emailService.sendEmail(this.nombre, this.email).subscribe({
       next: () => alert('Email enviado correctamente'),
